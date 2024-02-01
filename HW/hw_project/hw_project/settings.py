da7b32a51371b36e7ff95af10623f29bba98ee2a
Fiodar <fiodarspesian@mail.ru>
2024-02-01 11:41:25 +0300

    add localhost to settings.py-> ALLOWED_HOSTS = []

--- HW/hw_project/hw_project/settings.py
+++ HW/hw_project/hw_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'whip7$5z5y50xx&p+pniv$+1pt2d%jy222_9l@pe9knz#6oe66'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
