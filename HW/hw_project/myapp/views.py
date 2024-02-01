ff8a10812c9818ded93d3b084d5fdcbfa41bb47e
Fiodar <fiodarspesian@mail.ru>
2024-02-01 13:39:44 +0300

    add logger to project, correct settings for using logger

--- HW/hw_project/myapp/views.py
+++ HW/hw_project/myapp/views.py
@@ -1,12 +1,17 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
+logger = logging.getLogger(__name__)
+
 
 def index(request):
+    logger.info('Index page accessed')
     return render(request, 'index.html', )
 
 
 def about(request):
+    logger.debug('About page accessed')
     return render(request, 'about.html')
 
 
