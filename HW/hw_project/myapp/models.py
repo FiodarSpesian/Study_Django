8be5600d7e2bc19790f80025ab81674442d2439a
Fiodar <fiodarspesian@mail.ru>
2024-02-05 15:50:42 +0300

    add 3 models, makemigrations, apply migrations

--- HW/hw_project/myapp/models.py
+++ HW/hw_project/myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=25)
+    adress = models.CharField(max_length=100)
+    regist_date = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField()
+    regist_prod_date = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
