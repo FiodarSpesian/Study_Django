314f458fc6be1b2c0a4b14b03276e8009ddb28bc
Fiodar <fiodarspesian@mail.ru>
2024-02-08 20:05:06 +0300

    add <__str__()> to models of database

--- HW/hw_project/myapp/models.py
+++ HW/hw_project/myapp/models.py
@@ -8,6 +8,10 @@ class User(models.Model):
     adress = models.CharField(max_length=100)
     regist_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Name: {self.name}\n Email: {self.email}\n ' \
+               f'Phone: {self.phone}\n Adress: {self.adress}\n'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -16,9 +20,15 @@ class Product(models.Model):
     count = models.IntegerField()
     regist_prod_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'Name: {self.name}\n Price: {self.price}'
+
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'hw_project/myapp/models.py.__str__(self):'
