6578298426b54d34ece95d5ed9286b3d7f17eacb
Fiodar <fiodarspesian@mail.ru>
2024-02-09 17:56:33 +0300

    try to order_by date 7/30/360

--- HW/hw_project/myapp/views.py
+++ HW/hw_project/myapp/views.py
@@ -2,6 +2,8 @@ import logging
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import User, Product, Order
+from datetime import datetime as dt
+from datetime import timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +21,8 @@ def about(request):
 def customer_products(request, customer_id, count_days):
     logger.info("Products page accessed")
     customer = get_object_or_404(User, pk=customer_id)
-    order = Order.objects.filter(customer_id=customer.pk)
+    order = Order.objects.filter(
+        customer_id=customer_id)  # order_by(date_ordered__gt=f'{dt.now() - timedelta(days=count_days)}')
     temp = []
     for item in order:
         temp.append(item.products.all())
@@ -30,8 +33,3 @@ def customer_products(request, customer_id, count_days):
     return render(request, 'myapp/customer_products.html', {'customer': customer,
                                                             'products': products,
                                                             'count_days': count_days})
-
-
-
-
-
