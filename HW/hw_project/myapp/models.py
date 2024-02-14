d66238a25e787e1ff7a5557d81013410f2988991
Fiodar <fiodarspesian@mail.ru>
2024-02-14 11:47:22 +0300

    add field image to product

--- HW/hw_project/myapp/models.py
+++ HW/hw_project/myapp/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     count = models.IntegerField()
     regist_prod_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(default=None)
 
     def __str__(self):
         return f'Name: {self.name} Price: {self.price}'
