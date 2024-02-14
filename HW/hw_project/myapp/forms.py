79444085e83522dd3b458094b6dfee46a4a660fd
Fiodar <fiodarspesian@mail.ru>
2024-02-14 12:21:09 +0300

    add file forms.py with ProductForm, correct settings.py, correct views.py

---
+++ HW/hw_project/myapp/forms.py
@@ -0,0 +1,11 @@
+import datetime
+from django import forms
+
+
+class ProductFormWidget(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Введите название продукта'}))
+    description = forms.Textarea()  #widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'user@mail.ru'}))
+    price = forms.DecimalField(max_digits=8, decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    count = forms.IntegerField(min_value=0, widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Введите количество товара'}))
+    image = forms.ImageField()
--- HW/hw_project/myapp/views.py
+++ HW/hw_project/myapp/views.py
@@ -2,8 +2,8 @@ import logging
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import User, Product, Order
-from datetime import datetime as dt
-from datetime import timedelta
+from .forms import ProductFormWidget
+from django.core.files.storage import FileSystemStorage
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +22,7 @@ def customer_products(request, customer_id, count_days):
     logger.info("Products page accessed")
     customer = get_object_or_404(User, pk=customer_id)
     order = Order.objects.filter(
-        customer_id=customer_id)  # order_by(date_ordered__gt=f'{dt.now() - timedelta(days=count_days)}')
+        customer_id=customer_id)  # order_by(date_ordered__gt=f'{datetime.now() - timedelta(days=count_days)}')
     temp = []
     for item in order:
         temp.append(item.products.all())
@@ -33,3 +33,26 @@ def customer_products(request, customer_id, count_days):
     return render(request, 'myapp/customer_products.html', {'customer': customer,
                                                             'products': products,
                                                             'count_days': count_days})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductFormWidget(request.POST)
+        message = 'Ошибка в данных'
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            price = form.cleaned_data['price']
+            count = form.cleaned_data['count']
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+            logger.info(f'Получили товар: {name=}, {price=}, {count=}.')
+            print(f'Получили {name=}, {price=}, {count=}.')
+            product = Product(name=name, description=description, price=price, count=count, image=image)
+            product.save()
+            message = 'Пользователь сохранён'
+    else:
+        form = ProductFormWidget()
+        message = 'Заполните форму'
+    return render(request, 'myapp/add_product_form.html', {'form': form, 'message': message})
